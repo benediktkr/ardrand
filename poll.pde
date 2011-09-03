7a5d9f46b85fec16b7ee2d0a129192abbab42f20
Benedikt Kristinsson <benedikt.k@gmail.com>
2011-09-03 14:16:11 +0000

    poll

--- poll.pde
+++ poll.pde
@@ -1,14 +1,12 @@
-void setuo() {
+void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
     char *in = (char*)malloc(6);
-    int i, pin;
-    for(i=0; i<5; i++) {
-        in[i] = Serial.read();
-    }
-    in[5] Serial.read();
-    pin = (int)in[5] % 6;
-    Serial.println(analogRead(pin));
+    int i, p;
+    for(i=0; i<5; i++)
+        Serial.read();
+    p = (int)Serial.read();
+    Serial.println(analogRead(p));
 }
--- poll.py
+++ poll.py
@@ -1,26 +1,37 @@
+#!/usr/bin/python
 #coding: utf8
+
 from serial import Serial
 from serial import SerialException
 from time import sleep
+from datetime import datetime, time, date
 import sys
 
-# Inherit serial when im not sitting in a cramped coach
 class Arduino(Serial):
-    def poll(self, pin=3):
+    def poll(self, t, pin=3):
         self.write("POLL " + str(pin))
-        return int(self.readline()[:-2])
+        try:
+            return t(self.readline()[:-2])
+        except:
+            return self.poll(self, t, pin)
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
-        print "usage: poll.py tty_port n"
+    if len(sys.argv) < 3:
+        print "usage: poll.py tty_port wait_time"
         sys.exit()
-    port = "/dev/ttyUSB0"
-    pin = 4
 
+    l = []
     ard = Arduino(sys.argv[1], 9600)
-    # while True:
-    for i in xrange(0, int(sys.argv[2])):
-        print ard.poll()
-        sleep(0.5)
+    try:
+        while True:
+            l.append(ard.poll(str))
+            sleep(float(sys.argv[2]))
 
+    except KeyboardInterrupt:
+        filename = "data/" + str(date.today()) + "_" + str(datetime.time(datetime.now())) + "_" + sys.argv[2] + ".txt"
+        print "Ok, writing to " + filename
+        f = open(filename, 'w')
+        f.write('\n'.join(l))
+        f.close()
+        print "wrote " + str(len(l)) + " lines"
