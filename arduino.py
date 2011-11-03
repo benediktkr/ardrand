19f808a028a80ea2b8ca70463e22c4a6b4f6de9f
Benedikt Kristinsson <benedikt.k@gmail.com>
2011-11-03 18:19:02 +0000

    meanrand algorithm implemented

--- arduino.py
+++ arduino.py
@@ -4,9 +4,50 @@ from collections import defaultdict
 from math import floor, ceil
 
 class Arduino(Serial):
-    def poll(self, pin=3):
+    """The Arduino class communicates with the arduino board and
+    builds a string of random bits. Hold the state of the algs"""
+    
+    bufsize = 100
+    buffer = [0]*bufsize
+    meanval = 0.0
+    pin = 3
+
+    def __init__(self, p=3):
+        pin = p
+        for i range len(buffer):
+            past[i] = self.readint()
+
+        meanval = float(sum(buffer))/bufsize
+
+    def readint(self):
+        self.write("POLL " + str(pin))
+        return int(self.readline()[:-2])
+
+    def poll(self):
         self.write("POLL " + str(pin))
         return self.readline()[:-2]
+
+    def meanrand(self, n):
+        """Generator that generates n supposedly random bits"""
+        for i in xrange(n):
+            # Remove the old value, adjust mean, read new val, adjust again
+            meanval -= buffer[i%bufsize]/bufsize
+            buffer[i%bufsize] = self.readint()
+            meanval += buffer[i%bufsize]/bufsize
+            m = int(math.ceil(meanval))
+
+            """The von Neumann box. 00/11 → discard, 10 → 1, 01 → 0"""
+            while True:
+                b0 = 1 if self.readint() > m else 0
+                b1 = 1 if self.readint() > 0 else 0
+                if b0 == b1:
+                    continue
+                else:
+                    break
+            if b0 == 1:
+                yield 1
+            else:
+                yield 0
 
 class Data:
     def __init__(self, l):
@@ -23,7 +64,7 @@ class Data:
         m = int(floor(self.mean))
         factor = float(sum([freqs[a] for a in freqs if a <= m]))/self.count
         return (freqs, factor)
-
+    
     def correlation(self):
         corr = []
         for i in range(1, len(self.data)):
